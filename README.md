792247db74b04fb9aa17e0a9444259f4d3123026
Strongorange <74127841+Strongorange@users.noreply.github.com>
2021-07-09 11:18:38 +0900

    1-2~3 useInput

--- README.md
+++ README.md
@@ -12,3 +12,8 @@ return { value } =>> { value : value } 객체를 return!!!!!!!
 
 const name = useInput("Mr.");
 name 은 {value: "Mr."} 이라는 객체가 되어 꺼내쓸때 name.value = "Mr." 이 된다.
+
+useInput 에 validator 를 추가해주었는데 willUpdate 가 true 이고
+App 에서 넣어줄 validator 부분 인자가 function 이면 true, false 를 받아 willUpdate 를 true or false 로 설정해주고 willUpdate 가 true 인경우 setValue 를 통해 입력값을 value 로 설정해 줌
+
+App 에서 maxLen 이라는 validator function 을 만들고 useInput 인자로 전달해 검증기능을 사용한다
--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import "./App.css";
 import React, { useState } from "react";
 
-const useInput = (initialValue) => {
+const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
   const onChange = (event) => {
-    console.log(event.target);
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
   };
   return { value, onChange };
 };
 
 const App = () => {
-  const name = useInput("Mr.");
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mr.", maxLen);
+  const email = useInput("@");
   return (
     <div>
       <span>Hello</span>
       <input placeholder="이름" {...name} />
+      <input placeholder="이메일" {...email} />
     </div>
   );
 };
